ef466bbede4283f1e98e97346a69acf8d4c768f5
Jongwon Lee <Jongwon@Jongwons-MacBook-Pro.local>
2015-03-11 15:41:52 -0400

    Initial Commit

---
+++ RomoTouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RomoTouch
+//
+//  Created by Jongwon Lee on 3/11/15.
+//  Copyright (c) 2015 Jongwon Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RomoTouch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RomoTouch
+//
+//  Created by Jongwon Lee on 3/11/15.
+//  Copyright (c) 2015 Jongwon Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
